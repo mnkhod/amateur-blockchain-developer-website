4fd87ea0c922d925678d4d2d2fd107479200a719
mnkhod <mnkhod.dev@gmail.com>
2022-06-18 03:13:12 +0800

    Updated Goal Description

--- src/src/components/home/Goal.jsx
+++ src/src/components/home/Goal.jsx
@@ -6,7 +6,7 @@ export default function Goal(){
       <p className="title uppercase text-blue-500 font-bold">Goal</p>
       <div className="flex flex-col items-center gap-y-3">
         <h2 className="uppercase text-xs md:text-4xl">Share Value & Impact Lives With Smart Contracts</h2>
-        <h2 className="uppercase text-gray-600 text-xs md:text-lg w-8/12">Platform is build to help developers transition from web 2.0 to web 3.0</h2>
+        <h2 className="uppercase text-gray-600 text-xs md:text-lg w-8/12">Platform to help developers transition from web 2.0 to web 3.0</h2>
         <h2 className="uppercase text-gray-600 text-xs w-4/12">amateur developer - mnkhod</h2>
       </div>
     </div>
