62f26ed2b469722144ac57e46ae4fd8b50bd0545
mnkhod <mnkhod.dev@gmail.com>
2022-06-19 14:13:29 +0800

    Added Exchange Link

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -10,17 +10,17 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="overflow-x-hidden w-screen min-h-screen flex flex-col">
-      <div className="flex flex-col md:flex-row justify-between px-0 md:px-24 items-center py-4 w-full border-b-4 border-black">
+    <div className="flex flex-col w-screen min-h-screen overflow-x-hidden">
+      <div className="flex flex-col items-center justify-between w-full px-0 py-4 border-b-4 border-black md:flex-row md:px-24">
         <img src={Logo} alt="logo" className="w-auto h-32 md:h-24" />
         <div className="flex flex-col md:flex-row gap-4">
-          <button type="button" class="nes-btn h-12 is-primary">Staking</button>
-          <button type="button" class="nes-btn h-12 is-warning">Exchange</button>
+          <button type="button" className="h-12 nes-btn is-primary">Staking</button>
+          <a href="https://exchange.mnkhod.xyz/" className="h-12 nes-btn is-warning">Exchange</a>
         </div>
       </div>
 
-      <div className="grow flex justify-center items-center my-12">
-        <div className="w-12/12 md:w-7/12 p-0 m-0 flex flex-col gap-y-20">
+      <div className="flex items-center justify-center my-12 grow">
+        <div className="flex flex-col p-0 m-0 w-12/12 md:w-7/12 gap-y-20">
           <Goal />
           <Services />
           <Socials />
